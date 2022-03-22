ebd3ad3b981d3e3f07c802195f0b8683498b59d8
Théophane Hufschmitt <theophane@hufschmitt.net>
2022-03-22 18:37:04 +0100

    First implementation
    Very simple, just takes a language name as argument and filters out
    everything that’s not a code block for this language

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,48 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+
 module Main where
 
+import Data.Text (Text)
+import Effect.Logging (Logging, stdErrorLogging, logError)
+import Neorg.Document
+import Neorg.Document.Tag (TagHandler, handleTag, handleSomeTag)
+import Optics.Core ((^.))
+import System.Environment (getArgs)
+import qualified Cleff
+import qualified Data.Text
+import qualified Data.Text.IO
+import qualified Neorg.Parser.Main as NeorgParser
+import qualified Type.Set
+
+type Convert = Cleff.Eff '[Logging, Cleff.IOE]
+
+type CodeTags = Type.Set.FromList '["code"]
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = Cleff.runIOE . stdErrorLogging $ do
+  args <- Cleff.liftIO getArgs
+  languageName <- case args of
+    [l] -> pure (Data.Text.pack l)
+    _ -> Cleff.liftIO $ fail "Supply one language name as argument" 
+  content <- Cleff.liftIO Data.Text.IO.getContents
+  parsedDocument <- NeorgParser.parse "stdin" content
+  case parsedDocument of
+    Left err -> logError err
+    Right (doc :: Document CodeTags) ->
+      Cleff.liftIO $ Data.Text.IO.putStrLn (collectCodeTags languageName doc)
+
+collectCodeTags :: Text -> Document CodeTags -> Text
+collectCodeTags tagName doc =
+  let blocks = doc ^. documentBlocks in
+  Data.Text.intercalate "\n" $ concatMap
+    (\block -> case block of
+      PureBlock (Tag t) -> handleSomeTag (handleCodeTag tagName) t
+      _ -> []
+    ) blocks
+
+handleCodeTag :: Text -> TagHandler CodeTags [Text]
+handleCodeTag tagName =
+  handleTag @"code" $ \language text -> if language /= Just tagName then [] else [text]
--- litterate-neorg.nix
+++ litterate-neorg.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, lib, neorg, text }:
+{ mkDerivation, base, cleff, lib, neorg, optics-core, text }:
 mkDerivation {
   pname = "litterate-neorg";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base neorg text ];
+  executableHaskellDepends = [ base cleff neorg optics-core text ];
   license = "unknown";
   hydraPlatforms = lib.platforms.none;
 }
